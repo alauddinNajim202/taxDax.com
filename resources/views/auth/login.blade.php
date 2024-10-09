c65847edb16ee9b66e392129a9e3edd019ccc6da
alauddin_najim <alauddinnajim202@gmail.com>
2024-10-09 17:50:48 +0600

    Front side mastering complete

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,14 +1,45 @@
-@extends('layouts.app')
+@extends('front_end.app')
+
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+    <!-- sign in-up section start -->
+    <section class="sign-in-up-common-section">
+        <div class="container">
+            <div class="sign-in-up-content-wrapper">
+                <div class="sign-in-up-image-area">
+                    <img src="{{ asset('front_end/assets/images/sign-in-banner.png') }}" alt="" srcset="">
+                </div>
+                <div class="sign-in-up-form-area">
+                    <div class="form-header-para">
+                        <h1>Sign in</h1>
+                        <p>New user? <a href="sign-up.html">Create an account</a></p>
+                    </div>
+                    <form class="tm-sign-in-up-form" method="POST" action="{{route('login')}}">
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
+                        @csrf
+
+
+                        <div class="form-floating">
+                            <input type="email" class="form-control" id="email" name="email" placeholder="name@example.com">
+                            <label for="email">Email address</label>
+                        </div>
+                        <div class="form-floating">
+                            <input type="password" class="form-control" id="password" name="password" placeholder="Password">
+                            <label for="password">Password</label>
+                        </div>
+                        <button type="submit">Sign In</button>
+                        <p class="tm-create-btn-link">Don't have an account? <a href="sign-up.html">Sign Up</a></p>
+                        <div class="or-wrapper">
+                            <div class="or-line"></div>
+                            <p class="or-text">or</p>
+                            <div class="or-line"></div>
+                        </div>
+                        <a class="google-link" href="#">
+                            <img src="assets/images/google.png" alt="" srcset="">
+                            Continue with Google</a>
+                    </form>
+
+                    {{-- <form method="POST" action="{{ route('login') }}">
                         @csrf
 
                         <div class="row mb-3">
@@ -64,10 +95,12 @@
                                 @endif
                             </div>
                         </div>
-                    </form>
+                    </form> --}}
+
                 </div>
             </div>
         </div>
-    </div>
-</div>
+    </section>
+
+    <!-- sign in-up section End -->
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Web\front_end\HomeController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -14,10 +15,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('front_end.layouts.home');
+//     // return view('welcome');
+// });
 
-// Auth::routes();
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/join-as-prepare', [HomeController::class, 'join_tax_prepare'])->name('home.prepare');
+
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+Auth::routes();
+
